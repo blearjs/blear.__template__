15518d40e063dc7704054fe4a575792b556010d9
cloudcome <cloudcome@163.com>
2016-05-17 15:56:16 +0800

    template

--- karma.config-gen.js
+++ karma.config-gen.js
@@ -54,12 +54,12 @@ module.exports = function (coverage) {
                 },
                 {
                     // 加载 node_modules 下的原始文件，但不直接引入，使用模块加载器引入
-                    pattern: './node_modules/blear-*/package.json',
+                    pattern: './node_modules/blear.*/package.json',
                     included: false
                 },
                 {
                     // 加载 node_modules 下的原始文件，但不直接引入，使用模块加载器引入
-                    pattern: './node_modules/blear-*/src/**',
+                    pattern: './node_modules/blear.*/src/**',
                     included: false
                 },
                 {
